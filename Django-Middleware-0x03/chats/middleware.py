1a553dc482e18f1ffd9d6718bf44d0c6e5928040
James <monokpe@gmail.com>
2025-07-23 13:41:00 +0100

    Add middleware that restricts access to the messaging between (PM and AM)

--- Django-Middleware-0x03/chats/middleware.py
+++ Django-Middleware-0x03/chats/middleware.py
@@ -1,13 +1,15 @@
 import logging
-from datetime import datetime
+from datetime import datetime, time
+from django.http import HttpResponseForbidden
 
-request_logger = logging.getLogger('request_logger')
+request_logger = logging.getLogger("request_logger")
+
 
 class RequestLoggingMiddleware:
     """
     Middleware to log request details including user, request path, and timestamp.
     """
-    
+
     def __init__(self, get_response):
         """
         One-time configuration and initialization.
@@ -22,14 +24,37 @@ class RequestLoggingMiddleware:
         request_logger.info(
             f"Request: {request.method} {request.path} at {datetime.now().isoformat()}"
         )
-        
+
         user = "Anonymous"
         if request.user.is_authenticated:
-            user = f"User_ID:{request.user.pk}" 
-        
+            user = f"User_ID:{request.user.pk}"
+
         log_message = f"User: {user} - Path: {request.path} - Method: {request.method}"
         request_logger.info(log_message)
-        response = self.get_response(request)
 
-        
+        response = self.get_response(request)
+        return response
+
+
+class RestrictAccessByTimeMiddleware:
+    def __init__(self, get_response):
+        """One-time configuration and initialization."""
+        self.get_response = get_response
+
+    def __call__(self, request):
+        """
+        This method is called for each request.
+        It checks the current time and denies access if outside allowed hours.
+        """
+        start_time = time(6, 0)
+        end_time = time(21, 0)  # 9:00 PM (21:00 in 24-hour format)
+
+        current_time = datetime.now().time()
+
+        if not (start_time <= current_time <= end_time):
+            return HttpResponseForbidden(
+                "Access to the service is restricted at this time."
+            )
+
+        response = self.get_response(request)
         return response
