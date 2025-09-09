656d3138c19633e57e369052a83a5c621553c399
James <monokpe@gmail.com>
2025-09-09 10:50:33 +0100

    Add and improve unit tests for utils module; enhance memoization test coverage

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%p%wy4dm7c%ex&x6y!0&#$gf6q1m_g9qimsnz2xvhg=^)2@$o#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
